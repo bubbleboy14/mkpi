17fe048ff6a69a4a0ed1e97845dde1f2118b26c4
bubbleboy14 <mario.balibrera@gmail.com>
2019-09-28 15:32:48 -0700

    prox handler

---
+++ mkpi/prox.py
@@ -0,0 +1,41 @@
+"""
+Prox (/prox) API:
+
+### action = pass (default)
+
+requires: "url"
+
+    - /prox?url=https://google.com
+
+returns: requested resource
+"""
+
+import requests
+from datetime import datetime, timedelta
+from cantools.web import respond, succeed, cgi_get
+from cantools.util import log
+from cantools import config
+
+CACHE = {}
+STAMP = {}
+limit = timedelta(seconds=int(config.prox.timeout))
+
+def check(url):
+	if url not in CACHE:
+		return False
+	elapsed = datetime.now() - STAMP[url]
+	log("elapsed: %s"%(elapsed,), 1)
+	return elapsed < limit
+
+def cache(url):
+	log("checking cache for: %s"%(url,))
+	if not check(url):
+		log("acquiring", 1)
+		CACHE[url] = requests.get(url).content
+		STAMP[url] = datetime.now()
+	return CACHE[url]
+
+def response():
+	succeed(cache(cgi_get("url")))
+
+respond(response)
