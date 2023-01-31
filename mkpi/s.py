5a98c25852499179db9a64861b84dc893cc2693c
bubbleboy14 <mario.balibrera@gmail.com>
2023-01-31 02:46:15 -0800

    /s request handler w/ u and k modes

---
+++ mkpi/s.py
@@ -0,0 +1,36 @@
+"""
+Short (/s) API:
+
+### requires: "u"
+
+    - /u=https://www.google.com
+
+which returns: shortened version of url
+
+### or: "k"
+
+    - /k=aXzHO
+
+which returns: 302 redirect
+"""
+
+from cantools.web import respond, succeed, cgi_get, local
+from cantools.util import log, token
+from model import Short
+
+def response():
+	u = cgi_get("u", required=False, decode=True)
+	k = cgi_get("k", required=False)
+	if u:
+		s = Short.query(Short.url == u).get()
+		if not s:
+			s = Short()
+			s.url = u
+			s.code = token(5)
+			s.put()
+		succeed(s.code)
+	elif k:
+		s = Short.query(Short.code.like("%s%%"%(k,))).get()
+		s and local("redir")(s.url)
+
+respond(response)
